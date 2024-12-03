a6652bdc9b19183b55bf0d156de5ca890581e348
Maks Diakiewicz <Maks.Diakiewicz@warwick.ac.uk>
2024-12-03 09:55:32 +0000

    Bare module file to be used in a main program, to generate the charge distribution and potential arrays using two subroutines

--- Gauss_Seidel_solver.f90
+++ Gauss_Seidel_solver.f90
@@ -54,13 +54,9 @@ MODULE grid_initialisation
         ! Sets up the charge distribution based on the pre-defined problems.
         
         IF (problem == 'null') THEN 
-            rho =0.0
+            
+        
         ELSE IF (problem == 'single') THEN
-            ! Sets the paramters of the problem.
-            mean_x = 0.0
-            mean_y = 0.0
-            std = 0.1
-
             ! Loops over the grid and sets the correct 
             ! charge distribution. Omitts the ghost cells. i.e
             ! 0 and nx + 1 (similarly for y). Loops from the top to bottom to 
@@ -68,16 +64,15 @@ MODULE grid_initialisation
             ! i.e the origin of the grid is in the bottom left, as per te he top
             ! left
 
+            rho = 0.0_REAL64
 
-            ! TODO: Set the input of the Gaussian using literals maybe.
-            ! TODO: Make sure that the looping is correct.
             DO j=ny,1,-1
                 DO i=1,nx
                     pos = position_converter(x_axis,y_axis,i,j)
                     x = pos(1)
                     y = pos(2)
     
-                    rho(i,j) = Gaussian(x,y,mean_x,mean_y,std)
+                    rho(i,j) = Gaussian(x,y,0.0_REAL64,0.0_REAL64,0.1_REAL64)
                 END DO
             END DO
         
@@ -85,8 +80,10 @@ MODULE grid_initialisation
             ! Uses the same looping convention as above. 
             ! This time the paramaters are set directly in the function call 
             ! to stop from creating more variables.
- 
-              DO j=ny,1,-1
+
+            rho = 0.0_REAL64
+            
+            DO j=ny,1,-1
                 DO i=1,nx
                     pos = position_converter(x_axis,y_axis,i,j)
                     x = pos(1)
@@ -122,18 +119,17 @@ MODULE grid_initialisation
 
         ALLOCATE(phi(0:nx + 1,0:ny + 1))
         
-        
+        ! Assumes that our boundary conditions for the potoential are 0 at the
+        ! edges. 
         phi(0:nx +1,0:ny + 1) = 0.0_REAL64
-
-
-
-        !phi(1:nx,1:ny)  = 1.0_REAL64
         
         counter = 0
+        
         DO WHILE (loop_flag)
            
               DO j=ny,1,-1
                 DO i=1,nx
+                    ! Implements the Gauss-Seidel algorithm as described in the pdf nootes
                     dphi_x = (phi(i+1,j) + phi(i-1,j))/(dx**2)
                     dphi_y = (phi(i,j+1) + phi(i,j-1))/(dy**2)
                     
@@ -145,11 +141,11 @@ MODULE grid_initialisation
             error = calc_error(rho,phi,dx,dy)
             
             counter = counter + 1
-            !PRINT*,phi
-            !PRINT*,error
+
+            ! Uses the same error tolerance as in the pdf notes.
             IF (error < 10E-5) THEN
-               
                 loop_flag = .FALSE.
+               
                 PRINT '(A," ",G0," ",A)',' The algorithm took', counter, 'iterations to converge'
             END IF
            
@@ -206,7 +202,7 @@ MODULE grid_initialisation
         error_tot = 0.0
         error_rms = 0.0
       
-
+        ! Implements the total error as in the pdf notes.
         DO j=ny,1,-1
             DO i=1,nx
                 error_tot = error_tot + ABS( -rho(i,j) & 
@@ -216,6 +212,7 @@ MODULE grid_initialisation
             END DO
         END DO
    
+        ! Implements the RMS error as in the pdf notes.
         DO j = ny,1,-1
             DO i=1,nx
                 error_rms = error_rms + ( (phi(i+1,j) -2.0_REAL64 * phi(i,j) + phi(i-1,j))/(dx**2) &
@@ -227,7 +224,8 @@ MODULE grid_initialisation
 
         ! Either error, may be 0. This is the case when there is no charge denisty,
         ! and the second derivatives of the potential dissappear. However, there still is 
-        ! a potential. 
+        ! a potential, as well as its first derivatives. Only need to handle the rms, since
+        ! it is in the denominator of the error calculation. 
       
 
         IF (error_rms <10E-14) THEN 
@@ -242,64 +240,4 @@ MODULE grid_initialisation
 END MODULE
 
 
-
-PROGRAM Solver
-    USE ISO_FORTRAN_ENV
-    USE domain_tools
-    USE grid_initialisation
-    USE command_line
-
-    IMPLICIT NONE
-
-    REAL(REAL64),DIMENSION(:,:), ALLOCATABLE :: rho
-    REAL(REAL64),DIMENSION(:,:), ALLOCATABLE :: phi
-    INTEGER :: nx,ny,i,fu,j
-    REAL(REAL64) :: dx,dy
-    CHARACTER(LEN=10) :: problem
-    LOGICAL :: arg
-
-    CHARACTER(LEN=*),PARAMETER :: OUT_FILE = 'potential.csv'
-    
-        
-    ! TODO: Check user input. Allocate exact string length for problem.
-    
-    CALL  parse_args
-    
-
-    ! Gets the requierd user inputs.
-    arg = get_arg('nx',nx)
-    arg = get_arg('ny',ny)
-    arg = get_arg('problem',problem)
-
-    fu = 50
-
-    CALL define_rho(rho,nx,ny,problem,dx,dy)
-    CALL calc_potential(rho,phi,dx,dy)
-    
-    ! Prints the charge distribution, depending on the user 
-    ! input 'problem'
-    
-    !PRINT*,rho(4,:)
-    
-    OPEN(fu,action = 'write',file = OUT_FILE,status = 'replace')
-
-    DO j=ny,1,-1
-        DO i=1,nx
-            WRITE(fu,'(F10.5)',ADVANCE = 'NO') phi(i,j)
-            IF (i < nx) THEN
-                WRITE(fu, '(A)', ADVANCE='NO') ','
-            END IF
-        END DO
-        WRITE(fu,*)
-    END DO
-        
-      
-
-    CLOSE(fu)
-
-END PROGRAM
-
-
-        
-          
    
